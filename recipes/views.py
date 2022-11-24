3f2fe3515ffeaea6fb39c154fc5d55b08061901c
Guilherme Costa <guilherme.partic@gmail.com>
2022-11-24 19:12:23 -0300

    alterei o css

--- recipes/views.py
+++ recipes/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def home(request) -> render:
     return render(request, 'recipes/pages/home.html')
+
+
+def recipes(request, id) -> render:
+    return render(request, 'recipes/pages/recipe-view.html')
