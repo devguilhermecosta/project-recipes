e720b714fb7e77ff1c61f837354ef53d7f9e8d2d
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2022-12-15 23:15:40 -0300

    I updated the register.html

---
+++ authors/forms.py
@@ -0,0 +1,13 @@
+from django.forms import ModelForm
+from django.contrib.auth.models import User
+
+
+class RegisterForm(ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            'first_name',
+            'last_name',
+            'username',
+            'password',
+        ]
--- authors/views.py
+++ authors/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .forms import RegisterForm
 
 
 def register_view(request) -> render:
-    return render(request, 'authors/pages/author.html')
+    form: RegisterForm = RegisterForm()
+
+    return render(request, 'authors/pages/author.html', context={
+        'form': form,
+    })
