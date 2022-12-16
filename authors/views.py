1ff038e119d5963cea8f31ae680aa055bd2a6dda
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2022-12-15 23:42:25 -0300

    Update views.py

--- authors/views.py
+++ authors/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpRequest
 from .forms import RegisterForm
 
 
-def register_view(request) -> render:
-    form: RegisterForm = RegisterForm()
+def register_view(request: HttpRequest) -> render:
+    if request.POST:
+        form: RegisterForm = RegisterForm(request.POST)
+    else:
+        form = RegisterForm()
 
     return render(request, 'authors/pages/author.html', context={
         'form': form,
