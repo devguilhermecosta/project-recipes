6bd612d7d90496094cf6f11d3437169adbcd491c
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2022-12-26 16:04:53 -0300

    Updated RegusterForm

--- authors/forms.py
+++ authors/forms.py
@@ -1,8 +1,34 @@
 from django import forms
+from django.forms import CharField
 from django.contrib.auth.models import User
 
 
+def add_placeholder(field: CharField, placeholder_val: str | int) -> None:
+    field.widget.attrs['placeholder'] = f'{placeholder_val}'
+
+
+def add_widget_attr(field: CharField, attr_name: str, attr_value: str) -> None:
+    field.widget.attrs[attr_name] = f'{attr_value}'
+
+
 class RegisterForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        add_widget_attr(self.fields['password'], 'placeholder', 'Digite sua senha')  # noqa: E501
+
+    password2 = forms.CharField(required=True,
+                                label='Confirme a senha',
+                                min_length=3,
+                                max_length=5,
+                                widget=forms.PasswordInput(attrs={
+                                    'placeholder': 'Repita a senha',
+                                    }),
+                                error_messages={
+                                    'invalid': 'Verifique seus dados',
+                                    'min_length': 'Deve ter pelo menos 3 caracteres',  # noqa: E501
+                                },
+                                )
+
     class Meta:
         model = User
         fields = [
@@ -20,6 +46,7 @@ class RegisterForm(forms.ModelForm):
 
         help_texts = {
             'username': 'Digite um usuário válido',
+            'password': 'A senha deve ter letras e números',
         }
 
         error_messages = {
@@ -37,8 +64,5 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'Digite seu nome',
                 'class': 'minha-classe',
                 'id': 'first_name',
-            }),
-            'password': forms.PasswordInput(attrs={
-                'placeholder': 'Digite sua senha',
             }),
         }
