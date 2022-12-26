5e2ed02fb73543cddaaffb462a298a981685ecfa
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2022-12-26 13:26:14 -0300

    Updated RegisterForm

--- authors/forms.py
+++ authors/forms.py
@@ -1,13 +1,44 @@
-from django.forms import ModelForm
+from django import forms
 from django.contrib.auth.models import User
 
 
-class RegisterForm(ModelForm):
+class RegisterForm(forms.ModelForm):
     class Meta:
         model = User
         fields = [
             'first_name',
             'last_name',
             'username',
+            'email',
             'password',
         ]
+
+        labels = {
+            'first_name': 'Nome',
+            'last_name': 'Sobrenome',
+        }
+
+        help_texts = {
+            'username': 'Digite um usuário válido',
+        }
+
+        error_messages = {
+            'email': {
+                'invalid': 'Digite um e-mail válido',
+                'required': 'Este campo é obrigatório',
+            },
+            'first_name': {
+                'min_length': 'Digite no mínimo 5 caracteres',
+            },
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Digite seu nome',
+                'class': 'minha-classe',
+                'id': 'first_name',
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Digite sua senha',
+            }),
+        }
