ac839e30a6504cc32ba16e04448bfb80ba78cff0
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-01-03 23:31:17 -0300

    created login tests

--- authors/tests/test_authors_urls.py
+++ authors/tests/test_authors_urls.py
@@ -19,23 +19,36 @@ class AuthorsUrlsTest(TestCase):
 
         self.assertEqual(response.status_code, 200)
 
+    def test_authors_url_status_code_404_if_method_get(self) -> None:
+        url: str = reverse('authors:register_create')
+        response: HttpResponse = self.client.get(url)
+
+        self.assertEqual(response.status_code, 404)
+
     def test_authors_url_register_create_is_correct(self) -> None:
         url: str = reverse('authors:register_create')
         self.assertEqual(url, '/authors/register/create/')
 
-    def test_authors_url_load_correct_template(self) -> None:
-        url: str = reverse('authors:register')
-
-        response: HttpResponse = self.client.get(url)
-
-        self.assertTemplateUsed(response, 'authors/pages/author.html')
-
-    def test_authors_url_login_is_correct(self) -> None:
+    def test_authors_url_login_status_code_200_ok(self) -> None:
         url: str = reverse('authors:login')
 
         response: HttpResponse = self.client.get(url)
 
         self.assertEqual(response.status_code, 200)
 
-    def test_f(self):
-        self.fail('Continuar os testes de login e logout.')
+    def test_authors_login_create_is_correct(self) -> None:
+        url: str = reverse('authors:login_create')
+
+        self.assertEqual(url, '/authors/login/create/')
+
+    def test_authors_login_create_status_code_200_if_method_post(self) -> None:
+        url: str = reverse('authors:login_create')
+        data: dict = {'username': 'guilherme', 'password': 'Gui123456'}
+        response: HttpResponse = self.client.post(url, data=data, follow=True)
+
+        self.assertEqual(response.status_code, 200)
+
+    def test_authors_logout_is_correct(self) -> None:
+        url: str = reverse('authors:logout')
+
+        self.assertEqual(url, '/authors/logout/')
--- authors/tests/test_authors_view.py
+++ authors/tests/test_authors_view.py
@@ -1,50 +1,84 @@
 from django.http import HttpResponse
-from django.test import Client, TestCase
+from django.test import TestCase
 from django.urls import ResolverMatch, resolve, reverse
-
 from authors import views
 
 
-class AuthorsUrlsTest(TestCase):
-    def setUp(self) -> None:
-        self.client: Client = Client()
-        return super().setUp()
-
-    def test_authors_url_register_load_correct_view(self) -> None:
+class AuthorsLoginViewTest(TestCase):
+    def test_url_register_load_correct_view(self) -> None:
         url: ResolverMatch = resolve(
             reverse('authors:register')
         )
         self.assertEqual(url.func, views.register_view)
-        ...
 
-    def test_authors_url_register_load_correct_template(self) -> None:
+    def test_url_register_load_correct_template(self) -> None:
         url: str = reverse('authors:register')
         response: HttpResponse = self.client.get(url)
 
         self.assertTemplateUsed(response, 'authors/pages/author.html')
 
-    def test_authors_url_register_create_load_correct_view(self) -> None:
+    def test_url_register_create_load_correct_view(self) -> None:
         url: ResolverMatch = resolve(
             reverse('authors:register_create')
             )
         self.assertEqual(url.func, views.register_create)
 
-    def test_authors_url_status_code_404_if_method_get(self) -> None:
-        url: str = reverse('authors:register_create')
-        response: HttpResponse = self.client.get(url)
-
-        self.assertEqual(response.status_code, 404)
-
-    def test_authors_url_login_load_correc_view(self) -> None:
+    def test_url_login_load_correc_view(self) -> None:
         response: ResolverMatch = resolve(
             reverse('authors:login')
         )
 
         self.assertEqual(response.func, views.login_view)
 
-    def test_authors_url_login_load_correct_template(self) -> None:
+    def test_url_login_load_correct_template(self) -> None:
         url: str = reverse('authors:login')
 
         response: HttpResponse = self.client.get(url)
 
         self.assertTemplateUsed(response, 'authors/pages/login.html')
+
+    def test_url_login_create_load_correct_view(self) -> None:
+        response: ResolverMatch = resolve(
+            reverse('authors:login_create')
+        )
+
+        self.assertEqual(response.func, views.login_create)
+
+    # def test_login_create_user_is_authenticated(self) -> None:
+    #     user: dict = {
+    #         'first_name': 'adão',
+    #         'last_name': 'da silva',
+    #         'username': 'adao123',
+    #         'email': 'adao@email.com',
+    #         'password': 'Adao1234@@',
+    #         'password2': 'Adao1234@@',
+    #     }
+
+    #     user_data: dict = {
+    #         'username': 'adao123',
+    #         'password': 'Adao1234@@',
+    #     }
+
+    #     response_1: HttpResponse = self.client.post(  # noqa: F841
+    #         reverse('authors:register_create'),
+    #         data=user,
+    #         follow=True,
+    #         )
+
+    #     response_2: HttpResponse = self.client.post(  # noqa: F841
+    #         reverse('authors:login_create'),
+    #         data=user_data,
+    #         follow=True,
+    #         )
+
+    def test_url_logout_load_correct_view(self) -> None:
+        response: ResolverMatch = resolve(
+            reverse('authors:logout')
+        )
+
+        self.assertEqual(response.func, views.logout_view)
+        self.fail(('finalizar os testes de login e logout. '
+                   'Para isso é preciso conseguir fazer o login. '
+                   'Finalizar também o manual de elaboração '
+                   'dos formulários.')
+                  )
